1f34b3218f039b38eca9aed92766bad8ef6edb5a
jacheampong <jacheampong@gmail.com>
2020-10-07 19:05:25 -0400

    switch between players & assign X and O to boxes

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ const winningMoves = []
 const playerOption = ['X', 'O']
 let player1 = ''
 let player2 = ''
+let player1Moves = []
+let player2Moves = []
+let currentPlayer = ''
 
-let boardMoves = new Array(9)
+let boardMoves = []
 
 // get all boxes and set to ''
 let allBoxes = document.querySelectorAll('.box')
+
+const player1Display = document.querySelector('#player-one')
+const player2Display = document.querySelector('#player-two')
 
 function init() {
     console.log('Welcome to Tic Tac Toe!')
@@ -28,16 +34,17 @@ function init() {
 // Call the initialization function
 init()
 
-// function to reset game board
+// function to reset game board and set all boxes to empty
 function resetPlayBoard(event) {
     event.preventDefault();
     console.log('Game board reset!')
 
     allBoxes.forEach(box => {
-        // console.log(`box value: ${box.innerHTML}`)
-        box.innerHTML = ''
+        // set all boxes to E - empty 
+        box.innerHTML = 'E'
     })
-    boardMoves = []
+    // boardMoves = []
+    randomSelectPlayer()
 }
 
 let resetButton = document.querySelector('#restart')
@@ -58,6 +65,13 @@ function randomSelectPlayer() {
     }
     
     console.log(`player1: ${player1}  <==> player2: ${player2} `)
+    
+    // player1 is current player when game starts/restarted
+    currentPlayer = player1
+
+    // assign X and O to player1 and player2
+    player1Display.innerHTML = `Player 1: ${player1}`
+    player2Display.innerHTML = `Player 2: ${player2}`
 }
 
 // Based on the max length of the Array. Return a random items index
@@ -75,10 +89,15 @@ function selectBox() {
     mainContainer.addEventListener('click', (event) => {
 
       if (event.target.classList.contains('box')) {
-        allBoxes.forEach((elm, index) => {
-          if (elm === event.target) {
-            console.log(`Button Number ${index + 1} Clicked!`)
+        allBoxes.forEach((box, index) => {
+          if (box === event.target) {
+              console.log(`elm: ${box.value}`)
+
+            // set the button to currentPlayer - 'X' or 'O'
+            event.target.innerHTML = currentPlayer
             boardMoves.push(index + 1)
+            // switch player to next
+            switchPlayer()
           }
         })
       } else {
@@ -88,3 +107,8 @@ function selectBox() {
 }
 
 selectBox()
+
+// switch current player between player1 to player2 and vice versa
+function switchPlayer() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1
+}
